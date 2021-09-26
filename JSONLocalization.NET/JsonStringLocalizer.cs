6b6eeb4d49d551185f4fa9ef95d606844146f49a
Mukesh Murugan <iammukeshm@gmail.com>
2021-09-26 16:21:04 +0530

    updates

--- JSONLocalization.NET/JsonStringLocalizer.cs
+++ JSONLocalization.NET/JsonStringLocalizer.cs
@@ -9,100 +9,99 @@ using System.Threading;
 
 namespace JSONLocalization.NET
 {
-        public class JsonStringLocalizer : IStringLocalizer
+    public class JsonStringLocalizer : IStringLocalizer
+    {
+
+        private readonly IDistributedCache _cache;
+
+        private readonly JsonSerializer _serializer = new JsonSerializer();
+
+        public JsonStringLocalizer(IDistributedCache cache)
         {
-            private string Localization => "Resources";
+            _cache = cache;
+        }
 
-            private readonly IDistributedCache _cache;
-
-            private readonly JsonSerializer _serializer = new JsonSerializer();
-
-            public JsonStringLocalizer(IDistributedCache cache)
+        public LocalizedString this[string name]
+        {
+            get
             {
-                _cache = cache;
+                string value = GetString(name);
+                return new LocalizedString(name, value ?? name, value == null);
             }
+        }
 
-            public LocalizedString this[string name]
+        public LocalizedString this[string name, params object[] arguments]
+        {
+            get
             {
-                get
+                var actualValue = this[name];
+                return !actualValue.ResourceNotFound
+                    ? new LocalizedString(name, string.Format(actualValue.Value, arguments), false)
+                    : actualValue;
+            }
+        }
+
+        public IEnumerable<LocalizedString> GetAllStrings(bool includeParentCultures)
+        {
+            string filePath = $"Resources/{Thread.CurrentThread.CurrentCulture.Name}.json";
+            using (var str = new FileStream(filePath, FileMode.Open, FileAccess.Read, FileShare.Read))
+            using (var sReader = new StreamReader(str))
+            using (var reader = new JsonTextReader(sReader))
+            {
+                while (reader.Read())
                 {
-                    string value = GetString(name);
-                    return new LocalizedString(name, value ?? name, value == null);
-                }
-            }
-
-            public LocalizedString this[string name, params object[] arguments]
-            {
-                get
-                {
-                    var actualValue = this[name];
-                    return !actualValue.ResourceNotFound
-                        ? new LocalizedString(name, string.Format(actualValue.Value, arguments), false)
-                        : actualValue;
-                }
-            }
-
-            public IEnumerable<LocalizedString> GetAllStrings(bool includeParentCultures)
-            {
-                string filePath = $"{Localization}/{Thread.CurrentThread.CurrentCulture.Name}.json";
-                using (var str = new FileStream(filePath, FileMode.Open, FileAccess.Read, FileShare.Read))
-                using (var sReader = new StreamReader(str))
-                using (var reader = new JsonTextReader(sReader))
-                {
-                    while (reader.Read())
-                    {
-                        if (reader.TokenType != JsonToken.PropertyName)
-                            continue;
-                        string key = (string)reader.Value;
-                        reader.Read();
-                        string value = _serializer.Deserialize<string>(reader);
-                        yield return new LocalizedString(key, value, false);
-                    }
-                }
-            }
-
-            public IStringLocalizer WithCulture(CultureInfo culture)
-            {
-                CultureInfo.DefaultThreadCurrentCulture = culture;
-                return new JsonStringLocalizer(_cache);
-            }
-
-            private string GetString(string key)
-            {
-                string relativeFilePath = $"{Localization}/{Thread.CurrentThread.CurrentCulture.Name}.json";
-                string fullFilePath = Path.GetFullPath(relativeFilePath);
-                if (File.Exists(fullFilePath))
-                {
-                    string cacheKey = $"locale_{Thread.CurrentThread.CurrentCulture.Name}_{key}";
-                    string cacheValue = _cache.GetString(cacheKey);
-                    if (!string.IsNullOrEmpty(cacheValue)) return cacheValue;
-                    string result = PullDeserialize<string>(key, Path.GetFullPath(relativeFilePath));
-                    if (!string.IsNullOrEmpty(result)) _cache.SetString(cacheKey, result);
-                    return result;
-                }
-
-                return default(string);
-            }
-
-            private T PullDeserialize<T>(string propertyName, string filePath)
-            {
-                if (propertyName == null) return default(T);
-                if (filePath == null) return default(T);
-                using (var str = new FileStream(filePath, FileMode.Open, FileAccess.Read, FileShare.Read))
-                using (var sReader = new StreamReader(str))
-                using (var reader = new JsonTextReader(sReader))
-                {
-                    while (reader.Read())
-                    {
-                        if (reader.TokenType == JsonToken.PropertyName && (string)reader.Value == propertyName)
-                        {
-                            reader.Read();
-                            return _serializer.Deserialize<T>(reader);
-                        }
-                    }
-
-                    return default(T);
+                    if (reader.TokenType != JsonToken.PropertyName)
+                        continue;
+                    string key = (string)reader.Value;
+                    reader.Read();
+                    string value = _serializer.Deserialize<string>(reader);
+                    yield return new LocalizedString(key, value, false);
                 }
             }
         }
+
+        public IStringLocalizer WithCulture(CultureInfo culture)
+        {
+            CultureInfo.DefaultThreadCurrentCulture = culture;
+            return new JsonStringLocalizer(_cache);
+        }
+
+        private string GetString(string key)
+        {
+            string relativeFilePath = $"Resources/{Thread.CurrentThread.CurrentCulture.Name}.json";
+            string fullFilePath = Path.GetFullPath(relativeFilePath);
+            if (File.Exists(fullFilePath))
+            {
+                string cacheKey = $"locale_{Thread.CurrentThread.CurrentCulture.Name}_{key}";
+                string cacheValue = _cache.GetString(cacheKey);
+                if (!string.IsNullOrEmpty(cacheValue)) return cacheValue;
+                string result = GetValueFromJSON(key, Path.GetFullPath(relativeFilePath));
+                if (!string.IsNullOrEmpty(result)) _cache.SetString(cacheKey, result);
+                return result;
+            }
+
+            return default(string);
+        }
+
+        private string GetValueFromJSON(string propertyName, string filePath)
+        {
+            if (propertyName == null) return default;
+            if (filePath == null) return default;
+            using (var str = new FileStream(filePath, FileMode.Open, FileAccess.Read, FileShare.Read))
+            using (var sReader = new StreamReader(str))
+            using (var reader = new JsonTextReader(sReader))
+            {
+                while (reader.Read())
+                {
+                    if (reader.TokenType == JsonToken.PropertyName && (string)reader.Value == propertyName)
+                    {
+                        reader.Read();
+                        return _serializer.Deserialize<string>(reader);
+                    }
+                }
+
+                return default;
+            }
+        }
     }
+}
